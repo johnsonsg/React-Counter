831d689ac0426c72b0f270221fa64a6814dc0cbf
Shawn Johnson <johnson.sg@gmail.com>
2023-01-25 23:34:22 -0500

    setup

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function app() {
+  return (
+    <div>
+      <h1>Hello Webpack! This is a quick start BoilerPlate</h1>
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
 import { render } from 'react-dom'
+import App from './app'
 
-render(<h1>Hello Webpack! This is a quick start BoilerPlate</h1>, document.getElementById('app'))
+render(<App />, document.getElementById('app'))
