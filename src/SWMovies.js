11558b431ed359528ecfb23bee329633b7245694
Shawn Johnson <johnson.sg@gmail.com>
2023-01-27 09:35:29 -0500

    star wars api test

--- src/SWMovies.js
+++ src/SWMovies.js
@@ -1,11 +1,22 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 export default function SWMovies() {
   const [number, setNumber] = useState(1)
+  const [movie, setMovie] = useState('')
+  useEffect(() => {
+    async function getData() {
+      const response = await axios.get(`https://swapi.dev/api/films/${number}/`)
+      setMovie(response.data)
+      console.log(response)
+    }
+    getData()
+  }, [number]) // Only re-render when number changes.
   return (
     <div>
       <h1> Pick a Movie</h1>
-      <h4>You chose: {number}</h4>
+      <h4>{movie.title}</h4>
+      <p>{movie.opening_crawl}</p>
       <select value={number} onChange={e => setNumber(e.target.value)}>
         <option value='1'>1</option>
         <option value='2'>2</option>
@@ -13,7 +24,6 @@ export default function SWMovies() {
         <option value='4'>4</option>
         <option value='5'>5</option>
         <option value='6'>6</option>
-        <option value='7'>7</option>
       </select>
     </div>
   )
